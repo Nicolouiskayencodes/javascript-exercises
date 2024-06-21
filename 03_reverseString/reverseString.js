7a498f98bfceb4a5689ddfab4369bc24e0a2e509
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-06-20 18:42:24 -0700

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let reverse ="";
+  splitString = string.split("");
+  for (i=string.length; i>0; i--) {
+    reverse += splitString.pop();
+  }
+  return reverse;
 };
 
 // Do not edit below this line
