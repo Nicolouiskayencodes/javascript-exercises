aa77add29b42a16717237626ef2f23831c41c2c4
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-06-20 23:01:27 -0700

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(n,j) {
+  let sum = 0;
+ if (n < 0 || j < 0 || typeof(n) != "number" || typeof(j) != "number") {
+  return "ERROR";
+ } else if (n>j) {
+  let i = j;
+  while (i<=n) {
+    sum = sum +i;
+    i++;
+  }
+ } else {
+  let i = n;
+  while (i<=j) {
+    sum = sum + i;
+    i++;
+ }
+}
+return sum;
 };
 
 // Do not edit below this line
