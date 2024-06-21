9bc4a878ae589ae2ed0f690af9646e0b5ab19df6
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-06-20 22:20:21 -0700

    Complete exercise 04: Learn difference between for loop 'in' and 'of'

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+  let array = args.shift();
+  let removals = Array.from(args);
+  let newArray= [];
+  for (const val of array) {
+    if (!removals.includes(val)) {
+      newArray.push(val);
+    }
+  }
+  
+return newArray;
+
 };
 
 // Do not edit below this line
