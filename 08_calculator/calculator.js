d329c3b4b92da580619149702eeffcf45a426047
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-06-22 19:35:15 -0700

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    let newsum = array.reduce((b, item) => b + item, 0)
+    return newsum;
+ 
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multiple = array.reduce((a,b)=> a*b, 1)
+  return multiple;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let x=1
+	for (i=0; i<b; i++) {
+    x *= a
+  }
+  return x;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let x = 1;
+  for (i=a; i>0; i--) {
+    x *= i;
+  }
+  return x;
 };
 
 // Do not edit below this line
