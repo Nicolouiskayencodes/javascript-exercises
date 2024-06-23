9ed353857c2035f9fffc005553b32c83837d0760
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-06-22 21:04:41 -0700

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let alphanumerals = "abcdefghijklmnopqrstuvwxyvABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+  let array = string.split("")
+  let filteredarray = array.filter(function(item) {
+      return alphanumerals.includes(item);
+    })
+  let lowercasearray = filteredarray.map(function(item) {
+    return item.toLowerCase();
+  })
+  let backup = lowercasearray.map(function(item) {
+    return item;
+  })
+  let reversearray = lowercasearray.reverse();
+  if (backup.join("") == reversearray.join("")) {
+    return true;
+  } else {
+    return false;
+  }
+  
 };
 
 // Do not edit below this line
