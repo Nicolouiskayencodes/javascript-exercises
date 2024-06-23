23a9a6575b085e40f054ba0be7c7ddb151c32b18
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-06-22 22:47:01 -0700

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+  const x = [0, 1]
+  if (a<0) {
+    return "OOPS";
+  }
+  let b = parseInt(a);
+  for (i=0; i<=b; i++) {
+    if (i<2){
+      console.log(x[i])
+    } else {
+      x[i] = x[i-1] + x[i-2];
+      console.log(x[i])
+    }
+    
+  }
+  return x[b];
 };
 
 // Do not edit below this line
