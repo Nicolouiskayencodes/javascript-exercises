e29cbd3293ea6cff7987c833565daad3d444c2cf
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-06-22 23:05:39 -0700

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(obj) {
+  let sortedAge = obj.sort(function(a,b) {
+    if (!a.hasOwnProperty("yearOfDeath")) {
+      a["yearOfDeath"] = new Date().getFullYear();
+    }
+    if (!b.hasOwnProperty("yearOfDeath")) {
+      b["yearOfDeath"] = new Date().getFullYear();
+    }
+    let ageA = a["yearOfDeath"] - a["yearOfBirth"];
+    console.log(ageA);
+    let ageB = b["yearOfDeath"] - b["yearOfBirth"];
+    console.log(ageB);
+    if (ageA > ageB) {
+      return -1;
+    } else if (ageA < ageB) {
+      return 1;
+    } else {
+      return 0;
+    }
+  })
+  return sortedAge[0];
 };
 
 // Do not edit below this line
