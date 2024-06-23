175fb1d296fa089dab76cf17ea36767a334ba4b4
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-06-22 22:51:10 -0700

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+  let books = obj.map(function(item){
+    return item["title"];
+  })
+  return books;
 };
 
 // Do not edit below this line
